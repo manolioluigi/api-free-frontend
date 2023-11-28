6ee97632af1926a67b7a572775fc3d184e972da2
manolioluigi <luigi.manolio1999@gmail.com>
2023-11-28 13:35:09 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<script></script>
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <template>
   <div>
+    <Header></Header>
     <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './styles/generals.scss' as *;
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,104 @@
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      menuItems: [
+        {
+          label: "Home",
+          routeName: "home",
+        },
+        {
+          label: "Nuovo Post",
+          routeName: "createPost",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <nav
+      class="navbar navbar-expand-lg custom-header d-flex align-items-center padding-0"
+    >
+      <div class="container">
+        <!-- Logo -->
+
+        <div>
+          <router-link class="navbar-brand d-flex" :to="{ name: 'home' }">
+            <img src="../../public/logo.png" class="logo" alt="" />
+          </router-link>
+        </div>
+
+        <!-- Menu -->
+
+        <div>
+          <div class="d-none d-lg-block">
+            <ul class="navbar-nav">
+              <li v-for="(item, index) in menuItems" :key="index">
+                <router-link :to="{ name: item.routeName }" class="nav-link">
+                  {{ item.label }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+
+          <!-- Dropdown -->
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i class="fa-solid fa-bars white"></i>
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <ul class="list-unstyled mb-0 d-flex flex-column align-items-end container">
+              <li v-for="(item, index) in menuItems" :key="index">
+                <router-link :to="{ name: item.routeName }" class="nav-link">
+                  {{ item.label }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: sticky;
+  top: 0;
+  z-index: 3;
+}
+
+.custom-header {
+  background-color: rgb(34, 9, 44);
+  height: 10vh;
+}
+
+.dropdown-menu {
+  position: absolute;
+  right: 0;
+  background-color: rgb(135, 35, 65);
+}
+
+.navbar .nav-link {
+  color: white;
+
+  &:hover {
+    color: rgb(240, 89, 65);
+  }
+}
+
+.logo {
+  height: 8vh;
+  width: auto;
+}
+</style>
